f56fabfd12c19654743cdbe1a726022232e30a86
anya-decarlo <182010635+anya-decarlo@users.noreply.github.com>
2024-11-25 22:29:06 -0800

    add

--- clean_ms_data.sh
+++ clean_ms_data.sh
@@ -30,7 +30,7 @@ grep -v '^#' ms_data_dirty.csv | \
 sed '/^[[:space:]]*$/d' | \
 sed -e 's/,\+/,/g' | \
 sed -e 's/^,//g' -e 's/,$//g' | \
-cut -d ',' -f"$COLUMNS" |\
+cut -d ',' -f"$COLUMNS" > ms_data.csv
 #awk -F ',' '$5 >= 2.0 && $5 <= 8.0' > ms_data.csv
 
 # Create insurance.lst file 
