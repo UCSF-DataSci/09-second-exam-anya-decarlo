9893de5ca8905f1f8230afef17e2cd8be35ba603
anya-decarlo <182010635+anya-decarlo@users.noreply.github.com>
2024-11-25 19:43:31 -0800

    add

--- clean_ms_data.sh
+++ clean_ms_data.sh
@@ -5,7 +5,7 @@ touch ms_data.csv
 
 
 # Clean raw data and save to ms_data.csv
-grep -v '^#' ms_data_dirty.csv | \		# Remove comment lines
-sed '/^[[:space:]]*$/d' | \             # Remove empty lines
-sed -e 's/,\+/,/g' | \                  # Replace commas
-sed -e 's/^,//g' -e 's/,$//g'           # Remove leading/trailing commas 
+grep -v '^#' ms_data_dirty.csv | \
+sed '/^[[:space:]]*$/d' | \
+sed -e 's/,\+/,/g' | \
+sed -e 's/^,//g' -e 's/,$//g'
